9ffa092a66aede25e8e370df79e3230b0acfe563
asad <asadbahtiyarov2002@gmail.com>
2023-07-11 01:44:25 +0500

    dotenv load

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/AsaHero/abclinic/internal/app"
 	"github.com/AsaHero/abclinic/internal/pkg/config"
+	"github.com/joho/godotenv"
 	"go.uber.org/zap"
 )
 
 func main() {
+	// load dot env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("cannot Load .env: %v", err)
+	}
+
 	// config init
 	cfg := config.NewConfig()
 
@@ -18,13 +26,13 @@ func main() {
 	app := app.NewApp(cfg)
 
 	// app runs
-	go func ()  {
-		app.Logger.Info("Listen:", zap.String("address", cfg.Server.Host + cfg.Server.Port))
+	go func() {
+		app.Logger.Info("Listen:", zap.String("address", cfg.Server.Host+cfg.Server.Port))
 		if err := app.Run(); err != nil {
 			app.Logger.Error("error while running server: %v", zap.Error(err))
 		}
 	}()
-	
+
 	// wait for sigint
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
