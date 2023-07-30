0237941d07fcef4939076a5b1254d0eb8bdd59f4
asad <asadbahtiyarov2002@gmail.com>
2023-07-30 06:58:16 +0500

    ci/cd

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,21 @@
 package main
 
 import (
-	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/AsaHero/abclinic/internal/app"
 	"github.com/AsaHero/abclinic/internal/pkg/config"
-	"github.com/joho/godotenv"
 	"go.uber.org/zap"
 )
 
 func main() {
 	// load dot env file
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("cannot Load .env: %v", err)
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatalf("cannot Load .env: %v", err)
+	// }
 
 	// config init
 	cfg := config.NewConfig()
