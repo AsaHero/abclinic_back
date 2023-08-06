146267231069c5340df17b647d9dc4c21c9b56fd
asad <asadbahtiyarov2002@gmail.com>
2023-08-06 21:22:06 +0500

    Everything is ready to test

---
+++ api/models/auth.go
@@ -0,0 +1,20 @@
+package models
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+type RefreshTokenRequest struct {
+	RefreshToken string `json:"refresh_token"`
+}
+
+type RefreshTokenResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
---
+++ api/models/rbac.go
@@ -0,0 +1,43 @@
+package models
+
+type GetRolesResponse struct {
+	Roles []string `json:"roles"`
+}
+
+type GetUserInfoResponse struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Username  string `jsom:"username"`
+}
+
+type CreateUserRequest struct {
+	Role      string `json:"role"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Username  string `jsom:"username"`
+	Password  string `json:"password"`
+}
+
+type UpdateUserRequest struct {
+	Role      string `json:"role"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Username  string `jsom:"username"`
+	Password  string `json:"password"`
+}
+
+type GetAllUsersResponse struct {
+	GUID      string `json:"guid"`
+	Role      string `json:"role"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Username  string `jsom:"username"`
+}
+
+type GetUserResponse struct {
+	GUID      string `json:"guid"`
+	Role      string `json:"role"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Username  string `jsom:"username"`
+}
